ba27f5687af22c1085b1cf45f7830691e930c001
mavensingh <kuldeep.avsar@gmail.com>
2020-04-13 14:07:53 +0530

    creat bitmap with robotgo

---
+++ Robots-Automation/Bitmap/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/go-vgo/robotgo"
+)
+
+func main() {
+	bitmap := robotgo.CaptureScreen(10, 20, 30, 40)
+	defer robotgo.FreeBitmap(bitmap)
+
+	fmt.Println("...", bitmap)
+	fx, fy := robotgo.FindBitmap(bitmap)
+	fmt.Println("FindBitmap---------", fx, fy)
+	robotgo.SaveBitmap(bitmap, "test.png")
+}
